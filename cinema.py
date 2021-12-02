440e303e307fc8fba026e3c5b33ea4d9e7f79d80
afchatfield <72837913+afchatfield@users.noreply.github.com>
2021-12-02 11:18:08 +0000

    Finished Assignment

--- cinema.py
+++ cinema.py
@@ -1,5 +1,9 @@
 
 """
+cinema.py
+Find data from listchallenges and return csv file in data directory
+Author: Anthony Chatfield
+
 The page shown at the link below contains movies with some metadata of interest to our friend Quentin, a budding movie director.
 
 https://www.listchallenges.com/100-must-see-movies-for-more-advanced-cinephiles
@@ -24,6 +28,64 @@ Usage:
 python cinema.py --format [ CSV | HTML ]
 
 """
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-def transform():
-  pass
+#define paths for chrome.exe and chromedriver.exe
+chrome_path = "C:\Program Files\Google\Chrome\Application\Chrome.exe"
+chromedriver_path = "C:\Windows\chromedriver"
+service = Service(chromedriver_path)
+
+#define webdriver options
+chrome_options = webdriver.ChromeOptions()
+#specify chrome options
+chrome_options.binary_location = chrome_path
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+def getSoup(wd, website):
+		"""get html content of a website"""
+		wd.get(website)
+		return BeautifulSoup(wd.page_source, 'html.parser')
+
+def movieAndYear(input):
+		"""Gets (Movie, Year) from ""movie (year)" string input"""
+		return (input[:-7], int(input[-6:][1:5]))
+
+def getMoviesFromSite(wd, website):
+  """scrapes listchallenges stats website for movies in list"""
+  movies = []
+  soup = getSoup(wd, website+"/vote")
+  for a in soup.findAll('div', "listVote-item small-tall"):
+    #get title, strip whitespace
+    title = a.find('div', "item-name").text.strip()
+    movieYear = movieAndYear(title) #separate title and year
+    #get rank, take away #
+    rank = int(a.find('td', "listVoteItem-rank").text.strip()[1:])
+    #get num of up and down votes and add together
+    no_of_votes = int(a.find('span', "listVote-upVoteCount").text.strip()) + int(a.find('span', "listVote-downVoteCount").text.strip())
+    movies.append({'title': movieYear[0], 'year': movieYear[1],
+    'rank': rank, 'votes': no_of_votes})
+  return movies
+
+def transform(website):
+  #try to instantiate driver and get movies list of dictionaries
+  try:
+    wd = webdriver.Chrome(service=service, options=chrome_options) # , desired_capabilities=capabilities)
+    movies = getMoviesFromSite(wd, website)
+  finally:
+    #if error, quit the driver
+    wd.quit()
+  moviesdf = pd.DataFrame(movies)
+  moviesdf.set_index('rank')
+  return moviesdf
+  
+if __name__ == "__main__":
+  website = "https://www.listchallenges.com/100-must-see-movies-for-more-advanced-cinephiles"
+  df = transform(website)
+  #convert to csv file
+  df.to_csv('data/cinema.csv')
+
--- exercise.py
+++ exercise.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from statistics import stdev
 
 # Logic
 
@@ -9,7 +11,8 @@ def check_uniqueness(lst):
     Check if a list contains only unique values.
     Returns True only if all values in the list are unique, False otherwise
     """
-    pass
+    return True if len(set(lst))==len(lst) else False
+
 
 
 def smallest_difference(array):
@@ -19,7 +22,15 @@ def smallest_difference(array):
     Please note that the array can be large and that the more
     computationally efficient the better
     """
-    pass
+    array = sorted(array) #sort array
+    #create arbitarily large number to compare
+    diff = 10**20
+    #compare adjacent pairs in list
+    for i in range((len(array)-1)):
+        curr = abs(array[i+1] - array[i])
+        if curr < diff:
+            diff = curr
+    return diff
 
 
 # Finance and DataFrame manipulation
@@ -33,7 +44,12 @@ def macd(prices, window_short=12, window_long=26):
     Assume simple moving average rather than exponential moving average
     The expected output is in the output.csv file
     """
-    pass
+    # # Get the 26-day SMA of prices
+    k = prices['SX5T Index'].rolling(window=window_short).mean()
+    # Get the 12-day SMA of prices
+    d = prices['SX5T Index'].rolling(window=window_long).mean()
+    prices['macd'] = k - d
+    return prices
 
 
 def sortino_ratio(prices):
@@ -43,8 +59,21 @@ def sortino_ratio(prices):
     Assume risk-free rate = 0
     On the given test set, it should yield 0.05457
     """
-    pass
-
+    #get daily returns of sx5t index
+    daily_return = prices['SX5T Index'].pct_change().dropna(axis=0)
+    #get rate of return
+    start = prices['SX5T Index'][0]
+    end = prices['SX5T Index'][-1]
+    # r =  (end - start) / start #actual rate of return
+    r = ((end / start) ** (1/21)) -1 #anualized rate of return
+    # r = daily_return.mean() # 2mean rate of return
+    #get downside deviation/risk
+    #not sure if daily_return-r or daily_return-stdev or just daily_return, no explicit MAR
+    negative_excess_return = [i for i in daily_return-stdev(daily_return) if i < 0] 
+    sq_excess = [i**2 for i in negative_excess_return]
+    downside_dev = np.sqrt(sum(sq_excess) / len(daily_return))
+    print(downside_dev)
+    return r / downside_dev
 
 def expected_shortfall(prices, level=0.95):
     """
@@ -52,7 +81,15 @@ def expected_shortfall(prices, level=0.95):
     returns the expected shortfall at a given level
     On the given test set, it should yield -0.03468
     """
-    pass
+    #get daily returns of sx5t index
+    daily_returns = prices.copy().pct_change().dropna(axis=0).rename(columns={'SX5T Index': 'dr1'})
+    #modify returns with a constant value
+    daily_returns['dr2'] = daily_returns['dr1'].copy()
+    daily_returns.loc[daily_returns['dr1'] < daily_returns['dr1'].quantile(level), 'dr2'] -= 0.02
+    #calc value at risk of daily returns
+    var = daily_returns.quantile(1-level, axis=0, interpolation='higher')
+    #get ES: average of worst losses (under var)
+    return daily_returns[daily_returns.lt(var, axis=1)]['dr1'].mean()
 
 
 # Plot
@@ -63,4 +100,11 @@ def visualize(prices, path):
     Code a function that takes a DataFrame named prices and
     saves the plot to the given path
     """
-    pass
+    plt.figure()
+    df.plot()
+    plt.savefig(path)
+
+df = pd.read_csv('data/data.csv').set_index('date')
+df.index = pd.to_datetime(df.index)
+print(sortino_ratio(df))
+# visualize(df, "data/plot.jpg")
